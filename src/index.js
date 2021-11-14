03738f5458cde79e86a7f3713a179bd88bebf8f6
lystun <okegbemiolatunde@gmail.com>
2021-11-14 11:36:00 +0100

    Force redirect to https

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./state/store";
+import HttpsRedirect from 'react-https-redirect';
 
 ReactDOM.render(
+  <HttpsRedirect>
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>{" "}
-  </React.StrictMode>,
+  </React.StrictMode>
+  </HttpsRedirect>,
   document.getElementById("root")
 );
